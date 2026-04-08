e7d4aafe379a6aa4e56f2bf1a2f7b1e088f4ea29
lovelyne08 <mitchellovelyne@gmail.com>
2026-04-08 17:01:41 +0000

    android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application")
+//    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -36,12 +37,17 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.activity:activity:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("com.google.android.gms:play-services-ads:23.0.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- app/src/main/java/com/lovelyne/chui_wellnessapp/MainActivity.kt
+++ app/src/main/java/com/lovelyne/chui_wellnessapp/MainActivity.kt
@@ -1,22 +1,43 @@
 package com.lovelyne.chui_wellnessapp
 
 import android.content.Intent
+import com.google.android.gms.ads.AdRequest
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class MainActivity : AppCompatActivity() {
+    //        a variable to store the ad once its loads
+
+    private var mInterstitialAd: InterstitialAd? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this)
+        val adView = findViewById<AdView>(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+        loadInterstitialAd()
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
 
 //        healthy recipe intent
@@ -24,32 +45,80 @@ class MainActivity : AppCompatActivity() {
 
 //        creating a variable to store the buttons
 
-        val recipe=findViewById<Button>(R.id.recipy)
+        val recipe = findViewById<Button>(R.id.recipy)
 //        set on click listener
-        recipe.setOnClickListener{
+        recipe.setOnClickListener {
 //            write your intents
-            val recipeIntent= Intent(applicationContext, HealthyRecipes::class.java)
+            val recipeIntent = Intent(applicationContext, HealthyRecipes::class.java)
             startActivity(recipeIntent)
+
+//            display the ad
+            showInterstitialAd()
         }
 
 //        nutrition intent
 
-        val nutrition=findViewById<Button>(R.id.nutrition)
+        val nutrition = findViewById<Button>(R.id.nutrition)
 
         nutrition.setOnClickListener {
 
-            val nutritionIntent= Intent(applicationContext, NutritionAdvice::class.java)
+            val nutritionIntent = Intent(applicationContext, NutritionAdvice::class.java)
             startActivity(nutritionIntent)
         }
 
 //        meditation intent
 
-        val meditation=findViewById<Button>(R.id.meditation)
+        val meditation = findViewById<Button>(R.id.meditation)
 
         meditation.setOnClickListener {
-            val meditationIntent= Intent(applicationContext,Meditation::class.java)
+            val meditationIntent = Intent(applicationContext, Meditation::class.java)
             startActivity(meditationIntent)
         }
 
+//        weekly goals intent
+        val goals = findViewById<Button>(R.id.goals)
+
+        goals.setOnClickListener {
+            val goalsIntent = Intent(applicationContext, WeeklyGoals::class.java)
+            startActivity(goalsIntent)
+        }
+
+//        Check Progress intent
+        val progress = findViewById<Button>(R.id.progress)
+
+        progress.setOnClickListener {
+            val progressIntent = Intent(applicationContext, CheckProgress::class.java)
+            startActivity(progressIntent)
+        }
+
+
+    }
+
+    fun loadInterstitialAd() {
+        val adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(
+            this,
+            "ca-app-pub-3940256099942544/1033173712", // Test ID
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+
+                override fun onAdLoaded(ad: InterstitialAd) {
+                    mInterstitialAd = ad
+                }
+
+                override fun onAdFailedToLoad(error: LoadAdError) {
+                    mInterstitialAd = null
+                }
+            }
+        )
+    }
+
+    //Show Interstitial ad
+    fun showInterstitialAd() {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+        }
     }
 }
+
